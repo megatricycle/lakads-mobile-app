efb8dfa6d61fbb99b128fd95be714cf2722aa65e
Peter Rupa <megatricycle@gmail.com>
2017-03-31 15:55:02 +0800

    Switched linter to semistandard

--- App/Containers/LoginScreen.js
+++ App/Containers/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 import {
   View,
   ScrollView,
@@ -8,15 +8,14 @@ import {
   Image,
   Keyboard,
   LayoutAnimation
-} from 'react-native'
-import { connect } from 'react-redux'
-import Styles from './Styles/LoginScreenStyles'
-import {Images, Metrics} from '../Themes'
-import LoginActions from '../Redux/LoginRedux'
-import { Actions as NavigationActions } from 'react-native-router-flux'
+} from 'react-native';
+import { connect } from 'react-redux';
+import Styles from './Styles/LoginScreenStyles';
+import {Images, Metrics} from '../Themes';
+import LoginActions from '../Redux/LoginRedux';
+import { Actions as NavigationActions } from 'react-native-router-flux';
 
 class LoginScreen extends React.Component {
-
   static propTypes = {
     dispatch: PropTypes.func,
     fetching: PropTypes.bool,
@@ -28,75 +27,75 @@ class LoginScreen extends React.Component {
   keyboardDidHideListener = {}
 
   constructor (props) {
-    super(props)
+    super(props);
     this.state = {
       username: 'reactnative@infinite.red',
       password: 'password',
       visibleHeight: Metrics.screenHeight,
       topLogo: { width: Metrics.screenWidth }
-    }
-    this.isAttempting = false
+    };
+    this.isAttempting = false;
   }
 
   componentWillReceiveProps (newProps) {
-    this.forceUpdate()
+    this.forceUpdate();
     // Did the login attempt complete?
     if (this.isAttempting && !newProps.fetching) {
-      NavigationActions.pop()
+      NavigationActions.pop();
     }
   }
 
   componentWillMount () {
     // Using keyboardWillShow/Hide looks 1,000 times better, but doesn't work on Android
     // TODO: Revisit this if Android begins to support - https://github.com/facebook/react-native/issues/3468
-    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow)
-    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide)
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow);
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide);
   }
 
   componentWillUnmount () {
-    this.keyboardDidShowListener.remove()
-    this.keyboardDidHideListener.remove()
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
   }
 
   keyboardDidShow = (e) => {
     // Animation types easeInEaseOut/linear/spring
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
-    let newSize = Metrics.screenHeight - e.endCoordinates.height
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    let newSize = Metrics.screenHeight - e.endCoordinates.height;
     this.setState({
       visibleHeight: newSize,
       topLogo: {width: 100, height: 70}
-    })
+    });
   }
 
   keyboardDidHide = (e) => {
     // Animation types easeInEaseOut/linear/spring
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     this.setState({
       visibleHeight: Metrics.screenHeight,
       topLogo: {width: Metrics.screenWidth}
-    })
+    });
   }
 
   handlePressLogin = () => {
-    const { username, password } = this.state
-    this.isAttempting = true
+    const { username, password } = this.state;
+    this.isAttempting = true;
     // attempt a login - a saga is listening to pick it up from here.
-    this.props.attemptLogin(username, password)
+    this.props.attemptLogin(username, password);
   }
 
   handleChangeUsername = (text) => {
-    this.setState({ username: text })
+    this.setState({ username: text });
   }
 
   handleChangePassword = (text) => {
-    this.setState({ password: text })
+    this.setState({ password: text });
   }
 
   render () {
-    const { username, password } = this.state
-    const { fetching } = this.props
-    const editable = !fetching
-    const textInputStyle = editable ? Styles.textInput : Styles.textInputReadonly
+    const { username, password } = this.state;
+    const { fetching } = this.props;
+    const editable = !fetching;
+    const textInputStyle = editable ? Styles.textInput : Styles.textInputReadonly;
     return (
       <ScrollView contentContainerStyle={{justifyContent: 'center'}} style={[Styles.container, {height: this.state.visibleHeight}]} keyboardShouldPersistTaps='always'>
         <Image source={Images.logo} style={[Styles.topLogo, this.state.topLogo]} />
@@ -151,21 +150,20 @@ class LoginScreen extends React.Component {
         </View>
 
       </ScrollView>
-    )
+    );
   }
-
 }
 
 const mapStateToProps = (state) => {
   return {
     fetching: state.login.fetching
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     attemptLogin: (username, password) => dispatch(LoginActions.loginRequest(username, password))
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
